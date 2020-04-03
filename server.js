0ead5239a2f52bf3490b7feb0bd77dae59aa0b71
lionelherrerobattista <lionherrero@gmail.com>
2020-04-02 21:54:01 -0300

    heroku

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname + '/angularapp'));
-
 app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req,res)
-{
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
-
-console.log("Finalizo!!");
+console.log('Finalizo!!');
